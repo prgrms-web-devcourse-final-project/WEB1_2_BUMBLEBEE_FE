47a5a66216377f7d31ffa281d38288f9166d377d
JOEIH <joh73459@gmail.com>
2024-11-19 14:49:33 +0900

    💄 Design : 사용자 마이페이지 메인 화면 디자인 #6

--- src/layouts/HeaderWithTitle.tsx
+++ src/layouts/HeaderWithTitle.tsx
@@ -7,7 +7,7 @@ export interface TitleProps {
 
 const HeaderWithTitle = ({ title }: TitleProps) => {
   return (
-    <div className='fixed top-0 flex h-[132px] w-[375px] flex-col items-center bg-white'>
+    <div className='fixed top-0 z-[1000] flex h-[132px] w-[375px] flex-col items-center bg-white'>
       <div className='fixed top-[46px] w-[330px] flex-col'>
         <div className='flex h-[37px] w-[100%] items-center justify-between'>
           <img
---
+++ src/pages/UserMypage/components/LatestReservation.tsx
@@ -0,0 +1,62 @@
+interface Reservation {
+  name: string;
+  date: string;
+  time: string;
+  room: string;
+  people: number;
+  price: number;
+}
+
+const latestReservationCard: Reservation = {
+  name: 'ABC스터디룸',
+  date: '2024.06.08',
+  time: '14:00',
+  room: 'ROOM A',
+  people: 4,
+  price: 8000,
+};
+
+const LatestReservation = () => {
+  return (
+    <div className='flex h-auto w-[330px] flex-col rounded-[10px] bg-white p-[16px] shadow-[0_0_6px_0_rgba(0,0,0,0.25)]'>
+      <div className='flex items-center gap-[18px]'>
+        <img
+          src='https://modo-phinf.pstatic.net/20180304_61/1520159998510ED9Yt_JPEG/mosaSDaCsR.jpeg'
+          alt='스터디룸 사진'
+          className='h-[118px] w-[118px] object-cover'
+        />
+
+        <div className='flex w-[auto] flex-col gap-[7px]'>
+          <p className='text-[16px] font-semibold'>
+            {latestReservationCard.name}
+          </p>
+          <ul className='flex flex-col gap-[2px] text-[12px]'>
+            <li className='flex gap-[12px]'>
+              <p className='w-[46px]'>예약일</p>
+              <span className='font-medium'>{latestReservationCard.date}</span>
+            </li>
+            <li className='flex gap-[12px]'>
+              <p className='w-[46px]'>예약시간</p>
+              <span className='font-medium'>{latestReservationCard.time}</span>
+            </li>
+            <li className='flex gap-[12px]'>
+              <p className='w-[46px]'>예약된 룸</p>
+              <span className='font-medium'>{latestReservationCard.room}</span>
+            </li>
+            <li className='flex gap-[12px]'>
+              <p className='w-[46px]'>인원</p>
+              <span className='font-medium'>
+                {latestReservationCard.people}인
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <span className='self-end text-[14px] font-medium'>
+        {latestReservationCard.price}원
+      </span>
+    </div>
+  );
+};
+
+export default LatestReservation;
