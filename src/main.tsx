15f73c25b50819a719a98348d76914459cc84bce
eunjju2 <graceun309@gmail.com>
2024-11-19 09:46:46 +0900

    ✨ Feat : 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Router';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   </QueryClientProvider>,
 );
