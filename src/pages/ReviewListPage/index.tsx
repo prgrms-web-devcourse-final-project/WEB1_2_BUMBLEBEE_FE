6853803d6266840b2284875cb3f63ab52c911bb7
JOEIH <joh73459@gmail.com>
2024-11-21 09:30:53 +0900

    ✨ Feat : 리뷰 관리 페이지 라우터 연결 #25

---
+++ src/pages/ReviewListPage/index.tsx
@@ -0,0 +1,12 @@
+import HeaderWithTitle from '@layouts/HeaderWithTitle';
+import MainLayout from '@layouts/MainLayout';
+
+const ReviewListPage = () => {
+  return (
+    <MainLayout headerType='both'>
+      <HeaderWithTitle title='리뷰' />
+    </MainLayout>
+  );
+};
+
+export default ReviewListPage;
