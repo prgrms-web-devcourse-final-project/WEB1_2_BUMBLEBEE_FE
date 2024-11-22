f609b67c820218a3c8dda60f48035eb788e39632
JOEIH <joh73459@gmail.com>
2024-11-22 11:17:48 +0900

    💄 Design : 사용자 알림 페이지 마크업 구현 #33

---
+++ src/pages/UserNotiPage/components/UserNotiList.tsx
@@ -0,0 +1,57 @@
+import UpcomingNotiCard from './UserNotiCard';
+
+export interface UserNotification {
+  id: number;
+  type: string;
+  message: string;
+  reservationInfo: string;
+  price?: number;
+  createdAt: string;
+}
+
+const allNotification: UserNotification[] = [
+  {
+    id: 1,
+    type: 'upcoming',
+    message: '방문 24시간 전입니다.',
+    reservationInfo: '스터디랩 / ROOM A',
+    createdAt: '2024-11-14T00:00:00',
+  },
+  {
+    id: 2,
+    type: 'completeReservation',
+    message: '',
+    reservationInfo: '스터디랩 / ROOM A',
+    price: 8000,
+    createdAt: '2024-11-12T14:00:00',
+  },
+];
+
+const UserNotiList = () => {
+  const sortedNotification = [...allNotification].sort((b, a) => {
+    return +new Date(a.createdAt) - +new Date(b.createdAt);
+  });
+
+  return (
+    <>
+      {allNotification.length > 0 ? (
+        <div className='mt-4 flex w-[375px] flex-col justify-center gap-[13px]'>
+          {sortedNotification.map((item) => {
+            return (
+              <UpcomingNotiCard
+                key={item.id}
+                item={item}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className='mt-[47px] w-[375px] text-center text-[14px] font-normal text-subfont'>
+          알림이 없습니다.
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserNotiList;
--- src/pages/UserNotiPage/index.tsx
+++ src/pages/UserNotiPage/index.tsx
@@ -1,11 +1,13 @@
 import HeaderOnlyTitle from '@layouts/HeaderOnlyTitle';
 import MainLayout from '@layouts/MainLayout';
+import UserNotiList from './components/UserNotiList';
 
 const UserNotiPage = () => {
   return (
     <MainLayout>
       <HeaderOnlyTitle title='알림' />
       <hr className='fixed top-[93px] mx-[22.5px] h-0.5 w-custom border-0 bg-black' />
+      <UserNotiList />
     </MainLayout>
   );
 };
