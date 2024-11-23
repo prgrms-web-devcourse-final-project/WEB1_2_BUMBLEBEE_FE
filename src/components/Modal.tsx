1672f28ea8d3035ff803bac81cf12f446f8f8f9f
JOEIH <joh73459@gmail.com>
2024-11-23 19:46:25 +0900

    ✨ Feat : 모달창 메세지에 따라 높이 자동 조절되도록 설정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,11 +7,11 @@ interface ModalProps {
 const Modal = ({ message, cancelButton, confirmButton }: ModalProps) => {
   return (
     <div className='fixed left-0 top-0 z-[1100] flex h-[100%] w-[100%] items-center justify-center bg-[rgba(0,0,0,0.4)]'>
-      <div className='flex h-[140px] w-[280px] flex-col rounded-lg bg-[rgba(255,255,255,0.98)]'>
-        <div className='flex h-[calc(100%-44px)] items-center justify-center'>
+      <div className='flex h-auto min-h-[140px] w-[280px] flex-col rounded-lg bg-[rgba(255,255,255,0.98)]'>
+        <div className='flex h-auto min-h-[calc(100%-44px)] flex-grow items-center justify-center px-5 py-4'>
           {message}
         </div>
-        <div className='flex h-[44px] border-t-[0.5px] border-subfont'>
+        <div className='flex h-[44px] w-[100%] border-t-[0.5px] border-subfont'>
           <button
             type='button'
             className='w-[50%] border-r-[0.5px] border-subfont'
