e66d5cd2d870d9f7ff7232fe9d9304d19437bebf
JOEIH <joh73459@gmail.com>
2024-11-23 17:30:54 +0900

    💄 Design : 사업자 알림 페이지 마크업 구현 #38

--- src/pages/HostNotiPage/index.tsx
+++ src/pages/HostNotiPage/index.tsx
@@ -1,11 +1,13 @@
 import HeaderOnlyTitle from '@layouts/HeaderOnlyTitle';
 import MainLayout from '@layouts/MainLayout';
+import HostNotiList from './components/HostNotiList';
 
 const HostNotiPage = () => {
   return (
     <MainLayout>
       <HeaderOnlyTitle title='알림' />
       <hr className='fixed top-[93px] mx-[22.5px] h-0.5 w-custom border-0 bg-black' />
+      <HostNotiList />
     </MainLayout>
   );
 };
