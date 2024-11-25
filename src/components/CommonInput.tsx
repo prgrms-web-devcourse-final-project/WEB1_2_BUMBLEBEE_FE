a90b2108f4d0c57fb313d596e80d0c625ed2dfe1
JOEIH <joh73459@gmail.com>
2024-11-25 09:57:49 +0900

    ♻️ Refactor : CommonInput props 수정

--- src/components/CommonInput.tsx
+++ src/components/CommonInput.tsx
@@ -1,10 +1,18 @@
-interface InfoProps {
+interface InputProps {
   label: string;
   placeholder?: string;
   defaultValue?: string;
+  value?: string;
+  onChangeFunction?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const CommonInput = ({ label, placeholder, defaultValue }: InfoProps) => {
+const CommonInput = ({
+  label,
+  placeholder,
+  defaultValue,
+  value,
+  onChangeFunction,
+}: InputProps) => {
   return (
     <div className='mx-auto flex w-custom flex-col gap-1.5'>
       <p className='w-[100%] text-sm font-normal'>{label}</p>
@@ -12,6 +20,8 @@ const CommonInput = ({ label, placeholder, defaultValue }: InfoProps) => {
         className='main-input'
         placeholder={placeholder}
         defaultValue={defaultValue}
+        value={value}
+        onChange={onChangeFunction}
       />
     </div>
   );
