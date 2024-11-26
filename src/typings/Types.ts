b861a84ed942d57725666a950f8d3c8e9e8faf9f
JOEIH <joh73459@gmail.com>
2024-11-26 21:36:59 +0900

    🏷️ Types : 기본 타입 정의 추가
    추후 수정 예정

--- src/typings/Types.ts
+++ src/typings/Types.ts
@@ -18,10 +18,12 @@ export type Space = {
   rooms: Room[];
 };
 
+// 일반 사용자
 export interface Member {
+  // memberId: bigint;
   memberNickname: string;
   memberPhonenumber: string;
-  memberAge: bigint;
+  memberAge: number; // bigint
   memberSex: string;
   memberEmail: string;
   memberPwd: string;
@@ -30,53 +32,9 @@ export interface Member {
   deleteAt?: string;
 }
 
-export interface Review {
-  reviewId: string;
-  workPlaceId: bigint;
-  reviewContent: string;
-  reviewRating: number;
-  createdAt: string;
-  updateAt?: string;
-}
-
-export interface Reservation {
-  memberId: bigint;
-  studyroomId: bigint;
-  reservationName: string;
-  reservationPhoneNumber: string;
-  reservationState: number; // enum
-  numberOfPeople: bigint;
-  reservationPrice: bigint;
-  startTime: string;
-  endTime: string;
-  createdAt: string;
-  updateAt: string;
-}
-
-export interface StudyRoom {
-  workPlaceId: number;
-  title: string;
-  description: string;
-  createdAt: string;
-  updatedAt: string;
-  capacity: number;
-  price: number;
-}
-
-export interface WorkPlace {
-  businessId: number;
-  workplaceName: string;
-  workPlacePhoneNumber: string;
-  workPlaceDescription: string;
-  workPlaceAddress: string;
-  workPlaceStartTime: string;
-  workPlaceEndTime: string;
-  createdAt: string;
-  updatedAt?: string;
-  startSum?: string;
-}
-
+// 사업자
 export interface Business {
+  // businessId: bigint;
   businessName: string;
   businessPwd: string;
   businessEmail: string;
@@ -86,12 +44,105 @@ export interface Business {
   deleteAt?: string;
 }
 
+// 리뷰
+export interface Review {
+  reviewId: string;
+  memberId: bigint;
+  workPlaceId: bigint;
+  reviewContent: string;
+  reviewRating: number;
+  createdAt: string;
+  updateAt?: string;
+}
+
+// 예약
+export interface Reservation {
+  reservationId: bigint;
+  memberId: bigint;
+  studyroomId: bigint;
+  reservationName?: string;
+  reservationPhoneNumber?: string;
+  reservationState: number; // enum
+  numberOfPeople: number; // bigint
+  reservationPrice: number; // bigint
+  startTime: string;
+  endTime: string;
+  createdAt: string;
+  updateAt?: string;
+}
+
+// 스터디룸
+export interface StudyRoom {
+  studyroomId: bigint;
+  workPlaceId: bigint;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  capacity: number;
+  price: number;
+  studyRoomImage: string[]; // erd상에서 타입 미정
+}
+
+// 사업장
+export interface WorkPlace {
+  workPlaceId: bigint;
+  businessId: bigint;
+  workplaceName: string;
+  workplaceImage: string; // erd상에서 타입 미정
+  workPlacePhoneNumber: string;
+  workPlaceDescription: string;
+  workPlaceAddress: string;
+  workPlaceStartTime: string;
+  workPlaceEndTime: string;
+  createdAt: string;
+  updatedAt?: string;
+  startSum?: number; // bigint
+}
+
+// 결제
 export interface Payment {
   paymentId: bigint;
-  reservationId: number;
+  reservationId: bigint;
   orderId: string;
   orderName: string;
-  method: number;
+  method: number; // enum
   paymentKey: string;
   totalAmount: number;
+  requestedAt: string;
+  approvedAt: string;
+  status: number; // enum
+}
+
+// 사용자 알림
+export interface Alarm {
+  // id?: bigint; // erd상에서 타입 미정
+  memberId: bigint;
+  message?: string; // erd상에서 타입 미정
+  createdAt?: string; // erd상에서 타입 미정
+}
+
+// 사업자 알림
+export interface BusinessAlarm {
+  // key?: bigint; // erd상에서 타입 미정
+  businessId: bigint;
+  businessMessage: string;
+  createdAt: string;
+}
+
+// 채팅방
+export interface ChatRoom {
+  chatroomId: bigint;
+  memberId: bigint;
+  chatTitle: string;
+}
+
+// 채팅
+export interface Chat {
+  chatId: bigint;
+  chatroomId: bigint;
+  message: string;
+  createdAt: string;
+  readYn?: boolean;
+  deleteYn?: boolean;
 }
