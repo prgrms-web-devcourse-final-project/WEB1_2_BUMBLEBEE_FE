cd34bc50b6c3514c0e181832a5496507eb5b595a
eunjju2 <graceun309@gmail.com>
2024-11-27 22:05:32 +0900

    💄 Design : 취소/환불 규정 폰트 크기 수정 #42

--- src/pages/PaymentPage/components/CancelRule.tsx
+++ src/pages/PaymentPage/components/CancelRule.tsx
@@ -4,7 +4,7 @@ const CancelRule = () => {
   return (
     <div className='mx-auto mt-8 flex w-custom flex-col gap-4'>
       <DetailTitle title='취소 / 환불 규정' />
-      <div className='flex flex-col gap-2 text-xs'>
+      <div className='flex flex-col gap-2 text-sm'>
         <div className='flex items-center justify-between'>
           <span>이용 24시간 전까지</span>
           <span className='font-normal'>총 금액의 100% 환불</span>
