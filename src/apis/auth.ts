56e28ec4c80a829d855281c2b51901d16877091a
ryeong <oaoa0728@naver.com>
2024-11-28 09:30:44 +0900

    ♻️ Refactor : authInstance로 합치기 #8

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,5 +1,5 @@
 import { removeAuthToken, setAuthToken } from '@utils/auth';
-import { tokenInstance } from '.';
+import { authInstance } from '.';
 
 interface UserSignUpData {
   nickName: string;
@@ -24,12 +24,12 @@ interface BusinessSignUpData {
 
 // 사용자 회원가입
 export const postUserSignUp = async (user: UserSignUpData): Promise<void> => {
-  await tokenInstance.post('/api/v1/member/siginup', user);
+  await authInstance.post('/api/v1/member/siginup', user);
 };
 
 // 사용자 로그인
 export const postUserLogin = async (user: LoginData): Promise<void> => {
-  const response = await tokenInstance.post('/login/member', user);
+  const response = await authInstance.post('/login/member', user);
   const { accessToken } = response.data;
   setAuthToken(accessToken);
 };
@@ -38,12 +38,12 @@ export const postUserLogin = async (user: LoginData): Promise<void> => {
 export const postBusinessSignUp = async (
   business: BusinessSignUpData,
 ): Promise<void> => {
-  await tokenInstance.post('/api/v1/business/siginup', business);
+  await authInstance.post('/api/v1/business/siginup', business);
 };
 
 // 사업자 로그인
 export const postBusinessLogin = async (business: LoginData): Promise<void> => {
-  const response = await tokenInstance.post('/login/business', business);
+  const response = await authInstance.post('/login/business', business);
   const { accessToken } = response.data;
   setAuthToken(accessToken);
 };
@@ -51,5 +51,5 @@ export const postBusinessLogin = async (business: LoginData): Promise<void> => {
 // 로그아웃
 export const postLogOut = async (): Promise<void> => {
   removeAuthToken();
-  await tokenInstance.post('/logout');
+  await authInstance.post('/logout');
 };
