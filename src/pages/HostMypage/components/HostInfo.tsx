39e05ab176e077934c337685654baf39ed5a124a
JOEIH <joh73459@gmail.com>
2024-11-29 01:31:40 +0900

    💄 Design : 사용자 정보가 undefined일 때 글자 스타일 수정 #6 #9

--- src/pages/HostMypage/components/HostInfo.tsx
+++ src/pages/HostMypage/components/HostInfo.tsx
@@ -6,12 +6,14 @@ const HostInfo = () => {
   // 마이페이지 진입 시 토큰 유무(로그인 상태) 확인할 것
 
   return (
-    <div className='self-start pl-[33px] pt-[30px]'>
+    <div className='flex flex-col gap-1 self-start pl-[33px] pt-[30px]'>
       <div className='text-[32px] font-bold leading-none text-white'>
-        {data?.businessName || '닉네임을 불러올 수 없습니다.'}
+        {data?.businessName || (
+          <p className='text-xl'>닉네임을 불러올 수 없습니다.</p>
+        )}
       </div>
       <div className='pt-0 text-[14px] text-white'>
-        {data?.businessEmail || '이메일을 불러올 수 없습니다.'}
+        {data?.businessEmail || <p>이메일을 불러올 수 없습니다.</p>}
       </div>
     </div>
   );
--- src/pages/UserMypage/components/UserInfo.tsx
+++ src/pages/UserMypage/components/UserInfo.tsx
@@ -7,12 +7,14 @@ const UserInfo = () => {
   // 마이페이지 진입 시 토큰 유무(로그인 상태) 확인할 것
 
   return (
-    <div className='self-start pl-[33px] pt-[30px]'>
+    <div className='flex flex-col gap-1 self-start pl-[33px] pt-[30px]'>
       <div className='text-[32px] font-semibold leading-none text-white'>
-        {data?.nickName || '닉네임을 불러올 수 없습니다.'}
+        {data?.nickName || (
+          <p className='text-xl'>닉네임을 불러올 수 없습니다.</p>
+        )}
       </div>
       <div className='pt-0 text-[14px] text-white'>
-        {data?.email || '이메일을 불러올 수 없습니다.'}
+        {data?.email || <p>이메일을 불러올 수 없습니다.</p>}
       </div>
     </div>
   );
