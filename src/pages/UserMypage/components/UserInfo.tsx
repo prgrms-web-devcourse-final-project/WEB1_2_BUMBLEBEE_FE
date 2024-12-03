ef118afcd282efe83b2f73aa7ca2935221692a19
JOEIH <joh73459@gmail.com>
2024-12-04 02:13:56 +0900

    ♻️ Refactor : 사용자, 사업자 마이페이지에서 undefined 처리 방식 수정 #6 #9
    렌더링 전에 문구 먼저 보이는게 안 좋아 보여서 다시 수정

--- src/pages/UserMypage/components/UserInfo.tsx
+++ src/pages/UserMypage/components/UserInfo.tsx
@@ -7,16 +7,16 @@ const UserInfo = () => {
   // 마이페이지 진입 시 토큰 유무(로그인 상태) 확인할 것
 
   return (
-    <div className='flex flex-col gap-1 self-start pl-[24px] pt-[30px]'>
-      <div className='text-[32px] font-semibold leading-none text-white'>
-        {data?.nickName || (
-          <p className='text-2xl'>닉네임을 불러올 수 없습니다.</p>
-        )}
-      </div>
-      <div className='pt-0 text-[14px] text-white'>
-        {data?.email || <p>이메일을 불러올 수 없습니다.</p>}
-      </div>
-    </div>
+    <>
+      {data && (
+        <div className='flex flex-col gap-1 self-start pl-[24px] pt-[30px]'>
+          <div className='text-[32px] font-semibold leading-none text-white'>
+            {data.nickName}
+          </div>
+          <div className='pt-0 text-[14px] text-white'>{data.email}</div>
+        </div>
+      )}
+    </>
   );
 };
 
