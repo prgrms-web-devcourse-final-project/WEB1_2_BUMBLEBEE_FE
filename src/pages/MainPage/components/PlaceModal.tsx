86e5a758463044bd066149c022cfb37ed12fa56f
eunjju2 <graceun309@gmail.com>
2024-12-03 16:47:24 +0900

    💄 Design : 장소 모달 디자인 수정 #86

--- src/pages/MainPage/components/PlaceModal.tsx
+++ src/pages/MainPage/components/PlaceModal.tsx
@@ -1,5 +1,6 @@
 import { GetPositionWorkPlaceData } from '@typings/types';
 import { useEffect, useRef } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 interface PlaceModalProps {
   place: GetPositionWorkPlaceData;
@@ -19,7 +20,7 @@ const PlaceModal = (props: PlaceModalProps) => {
     document.addEventListener('mousedown', handleOutsideClose);
 
     return () => document.removeEventListener('mousedown', handleOutsideClose);
-  }, []);
+  }, [onClose]);
 
   return (
     <div className='fixed left-[50%] top-0 z-[1500] flex h-[100%] w-[375px] translate-x-[-50%] items-center justify-center bg-[rgba(0,0,0,0.4)]'>
@@ -27,23 +28,33 @@ const PlaceModal = (props: PlaceModalProps) => {
         ref={modalRef}
         className='flex h-auto min-h-[140px] w-[280px] flex-col rounded-lg bg-[rgba(255,255,255,0.98)] text-center'
       >
-        <div className='flex h-auto min-h-[140px] items-center justify-between px-4 py-5'>
-          <img
-            src={place.imageUrl}
-            alt='사업장 사진'
-            className='h-20 w-20 rounded-lg object-cover'
-          />
+        <div className='flex h-auto min-h-[145px] flex-col items-center justify-between gap-4 px-6 py-7'>
+          <div className='flex w-[232px] justify-between px-1'>
+            <img
+              src={place.imageUrl}
+              alt='사업장 사진'
+              className='h-[60px] w-[60px] rounded-lg object-cover'
+            />
+            <div className='flex w-[150px] flex-col items-start justify-center gap-[6px] text-sm'>
+              <div className='flex gap-[3px]'>
+                <FaMapMarkerAlt className='text-base leading-none' />
+                <p className='px-1 text-left font-medium leading-none'>
+                  {place.workplaceName}
+                </p>
+              </div>
 
-          <div className='flex w-[150px] flex-col items-start gap-[6px] text-sm'>
-            <p className='px-1 text-left font-medium'>{place.workplaceName}</p>
-            <p className='px-1 text-left text-xs'>{place.workplaceAddress}</p>
-            <button
-              type='button'
-              className='flex h-7 w-[150px] cursor-pointer items-center justify-center rounded-[4px] bg-primary text-xs text-white'
-            >
-              상세보기
-            </button>
+              <p className='px-1 text-left text-xs leading-none'>
+                {place.workplaceAddress}
+              </p>
+            </div>
           </div>
+
+          <button
+            type='button'
+            className='flex h-8 w-[232px] cursor-pointer items-center justify-center rounded-[4px] bg-primary text-xs text-white'
+          >
+            상세보기
+          </button>
         </div>
       </div>
     </div>
