0c11aad64d8e0b6798a2343bd989e01e5d0cf625
JOEIH <joh73459@gmail.com>
2024-12-04 11:13:03 +0900

    ✨ Feat : 사업자 정보 수정 페이지 구현 #54

--- src/pages/HostInfoEditPage/components/HostEditContainer.tsx
+++ src/pages/HostInfoEditPage/components/HostEditContainer.tsx
@@ -1,10 +1,14 @@
 import DeleteAccountButton from '@components/DeleteAccountButton';
 import { useState } from 'react';
 import Modal from '@components/Modal';
+import { useLocation } from 'react-router-dom';
+import { Business } from '@typings/types';
 import HostEditForm from './HostEditForm';
 import useDeleteBusiness from '../hooks/useDeleteBusiness';
 
 const HostEditContainer = () => {
+  const location = useLocation();
+  const business: Business = { ...location.state };
   const { mutate } = useDeleteBusiness();
 
   const [modalOpen, setModalOpen] = useState(false);
@@ -16,7 +20,7 @@ const HostEditContainer = () => {
   return (
     <>
       <div className='relative mt-14 flex w-[375px] flex-col'>
-        <HostEditForm />
+        <HostEditForm business={business} />
         <div className='absolute bottom-[-280px] left-[50%] translate-x-[-50%]'>
           <DeleteAccountButton onClickButton={() => setModalOpen(true)} />
         </div>
