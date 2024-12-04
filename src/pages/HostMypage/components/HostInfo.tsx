4196123d538acdd6af0da8daaecec0c5d2d5c1fb
JOEIH <joh73459@gmail.com>
2024-12-04 14:51:10 +0900

    ♻️ Refactor : 사업자 마이페이지 변수명 변경 #9

--- src/pages/HostMypage/components/HostInfo.tsx
+++ src/pages/HostMypage/components/HostInfo.tsx
@@ -1,19 +1,19 @@
 import useGetBusinessData from '../hooks/useGetBusinessData';
 
 const HostInfo = () => {
-  const { data } = useGetBusinessData();
+  const { business } = useGetBusinessData();
 
   // 마이페이지 진입 시 토큰 유무(로그인 상태) 확인할 것
 
   return (
     <>
-      {data && (
+      {business && (
         <div className='flex flex-col gap-1 self-start pl-[24px] pt-[30px]'>
           <div className='text-[32px] font-bold leading-none text-white'>
-            {data.businessName}
+            {business.businessName}
           </div>
           <div className='pt-0 text-[14px] text-white'>
-            {data.businessEmail}
+            {business.businessEmail}
           </div>
         </div>
       )}
