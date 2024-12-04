f9fdff98f59d3f9f5f738088655c354a9aa99dc5
eunjju2 <graceun309@gmail.com>
2024-12-04 14:37:10 +0900

    ✨ Feat : 스터디룸 id로 경로 설정 #14

--- src/pages/ReservationPage/index.tsx
+++ src/pages/ReservationPage/index.tsx
@@ -4,25 +4,27 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import ReservationBar from './components/ReservationBar';
 import ImageCarousel from './components/ImageCarousel';
 import RoomDetail from './components/RoomDetail';
+import useGetStudyroomDetail from './hooks/useGetStudyroomDetail';
 
 const ReservationPage = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const studyroomId = Number(searchParams.get('studyroomId'));
-  // const { data } = useGetStudyroomDetail(studyroomId);
-  const data = {
-    studyRoomId: 1,
-    workplaceId: 1,
-    workplaceName: '타임유스터디카페 민락점',
-    studyRoomName: 'Room A',
-    description: '조용하고 쾌적한 환경, 최대 4인 가능',
-    imageUrl: [
-      'https://elasticbeanstalk-ap-northeast-2-405894845535.s3.ap-northeast-2.amazonaws.com/타임유스터디카페 민락점/Room A/Room A-1.jpg',
-      'https://elasticbeanstalk-ap-northeast-2-405894845535.s3.ap-northeast-2.amazonaws.com/타임유스터디카페 민락점/Room A/Room A-2.jpg',
-    ],
-    price: 7000,
-    capacity: 4,
-  };
+  const { data } = useGetStudyroomDetail(studyroomId);
+  console.log(data);
+  // const data = {
+  //   studyRoomId: 1,
+  //   workplaceId: 1,
+  //   workplaceName: '타임유스터디카페 민락점',
+  //   studyRoomName: 'Room A',
+  //   description: '조용하고 쾌적한 환경, 최대 4인 가능',
+  //   imageUrl: [
+  //     'https://elasticbeanstalk-ap-northeast-2-405894845535.s3.ap-northeast-2.amazonaws.com/타임유스터디카페 민락점/Room A/Room A-1.jpg',
+  //     'https://elasticbeanstalk-ap-northeast-2-405894845535.s3.ap-northeast-2.amazonaws.com/타임유스터디카페 민락점/Room A/Room A-2.jpg',
+  //   ],
+  //   price: 7000,
+  //   capacity: 4,
+  // };
 
   return (
     <MainLayout>
