c9b745a511a4311490a09e81c3848f768f1d01ff
JOEIH <joh73459@gmail.com>
2024-12-04 10:26:21 +0900

    ✨ Feat : 사용자 탈퇴 api 연결 #54

--- src/components/DeleteAccountButton.tsx
+++ src/components/DeleteAccountButton.tsx
@@ -1,8 +1,13 @@
-const DeleteAccountButton = () => {
+interface DeleteAccountProp {
+  onClickButton: () => void;
+}
+
+const DeleteAccountButton = ({ onClickButton }: DeleteAccountProp) => {
   return (
     <button
       type='button'
       className='h-[23px] w-[100%] text-[12px] text-subfont underline active:text-black'
+      onClick={onClickButton}
     >
       탈퇴하시겠습니까?
     </button>
--- src/pages/UserInfoEditPage/components/UserEditContainer.tsx
+++ src/pages/UserInfoEditPage/components/UserEditContainer.tsx
@@ -1,20 +1,39 @@
 import DeleteAccountButton from '@components/DeleteAccountButton';
 import { useLocation } from 'react-router-dom';
 import { Member } from '@typings/types';
+import { useState } from 'react';
+import Modal from '@components/Modal';
 import UserEditForm from './UserEditForm';
+import useDeleteMember from '../hooks/useDeleteMember';
 
 const UserEditContainer = () => {
   const location = useLocation();
   const user: Member = { ...location.state };
-  console.log(user);
+
+  const { mutate } = useDeleteMember();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleDeleteAccountClick = () => {
+    mutate();
+  };
 
   return (
-    <div className='relative mt-14 flex w-[375px] flex-col'>
-      <UserEditForm user={user} />
-      <div className='absolute bottom-[-280px] left-[50%] translate-x-[-50%]'>
-        <DeleteAccountButton />
+    <>
+      <div className='relative mt-14 flex w-[375px] flex-col'>
+        <UserEditForm user={user} />
+        <div className='absolute bottom-[-280px] left-[50%] translate-x-[-50%]'>
+          <DeleteAccountButton onClickButton={() => setModalOpen(true)} />
+        </div>
       </div>
-    </div>
+      {modalOpen && (
+        <Modal
+          message='정말 탈퇴하시겠습니까?'
+          onCancelButtonClick={() => setModalOpen(false)}
+          onConfirmButtonClick={handleDeleteAccountClick}
+        />
+      )}
+    </>
   );
 };
 
