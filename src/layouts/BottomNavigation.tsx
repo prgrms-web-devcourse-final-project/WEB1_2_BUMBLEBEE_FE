4ecacbcc27b304bb805feb2cb7758fdc5909aef0
JOEIH <joh73459@gmail.com>
2024-12-05 13:24:58 +0900

    ✨ Feat : 네비게이션 연결 #97

--- src/layouts/BottomNavigation.tsx
+++ src/layouts/BottomNavigation.tsx
@@ -2,29 +2,50 @@ import { AiFillHome } from 'react-icons/ai';
 import { BsChatDots } from 'react-icons/bs';
 import { CgSearch } from 'react-icons/cg';
 import { BiUser } from 'react-icons/bi';
-import { useState } from 'react';
+import useAuthStore from '@store/authStore';
+import { getRole } from '@utils/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const BottomNavigation = () => {
-  const [activeTab, setActiveTab] = useState('홈');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { isLogin } = useAuthStore();
+  const role = getRole();
+  const myPagePath = role === 'ROLE_USER' ? '/user-page' : '/host-page';
+
   const navigationItems = [
-    { icon: <AiFillHome />, label: '홈' },
-    { icon: <BsChatDots />, label: '문의' },
-    { icon: <CgSearch />, label: '검색' },
-    { icon: <BiUser />, label: '마이페이지' },
+    { icon: <AiFillHome />, label: '홈', path: '/' },
+    { icon: <BsChatDots />, label: '문의', path: isLogin ? '/chat-list' : '/' },
+    { icon: <CgSearch />, label: '검색', path: '/search' },
+    { icon: <BiUser />, label: '마이페이지', path: isLogin ? myPagePath : '/' },
   ];
+
+  // 현재 경로를 기준으로 활성화된 탭 설정
+  const activeTab =
+    navigationItems.find((item) => item.path === location.pathname)?.label ||
+    '홈';
+
+  const handleMovePage = (path: string) => {
+    if (!isLogin && (path === '/chat-list' || path === myPagePath)) {
+      navigate(path);
+      toast.error('로그인 후 이용해주세요');
+      return;
+    }
+    navigate(path);
+  };
 
   return (
     <div className='fixed bottom-0 z-10 flex h-[94px] w-[375px] items-center justify-between border-t-[1px] border-t-subfont bg-white px-[30px] pb-[30px] pt-[18px]'>
-      {navigationItems.map(({ icon, label }) => (
+      {navigationItems.map(({ icon, label, path }) => (
         <button
           type='button'
           key={label}
           className={`flex w-[45px] cursor-pointer flex-col items-center ${
             activeTab === label ? 'text-primary' : 'text-focusColor'
           }`}
-          onClick={() => {
-            setActiveTab(label);
-          }}
+          onClick={() => handleMovePage(path)}
         >
           <div className='text-[20px]'>{icon}</div>
           <p className='mt-1 text-[10px]'>{label}</p>
