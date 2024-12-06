ac2cd62f54216a2adf05d21386e85fd46c459b2e
JOEIH <joh73459@gmail.com>
2024-12-06 10:50:19 +0900

    ✨ Feat : 헤더 로고 누르면 홈으로 이동 #104

--- src/layouts/HeaderNoTitle.tsx
+++ src/layouts/HeaderNoTitle.tsx
@@ -1,15 +1,18 @@
 import Logo from '@assets/images/roomit_logo.png';
 import { RiNotification3Line } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const HeaderNoTitle = () => {
   return (
     <div className='fixed top-0 z-[1000] flex h-[93px] w-[375px] flex-col items-center bg-white'>
       <div className='fixed top-[46px] flex h-[37px] w-[330px] items-center justify-between'>
-        <img
-          className='h-[15px] w-[50px]'
-          src={Logo}
-          alt='ROOM:IT 로고'
-        />
+        <Link to='/'>
+          <img
+            className='h-[15px] w-[50px]'
+            src={Logo}
+            alt='ROOM:IT 로고'
+          />
+        </Link>
         <RiNotification3Line className='h-[24px] w-[24px]' />
       </div>
     </div>
--- src/layouts/HeaderWithTitle.tsx
+++ src/layouts/HeaderWithTitle.tsx
@@ -1,5 +1,6 @@
 import Logo from '@assets/images/roomit_logo.png';
 import { RiNotification3Line } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 export interface TitleProps {
   title: string;
@@ -10,11 +11,13 @@ const HeaderWithTitle = ({ title }: TitleProps) => {
     <div className='fixed top-0 z-[1000] flex h-[132px] w-[375px] flex-col items-center bg-white'>
       <div className='fixed top-[46px] w-[330px] flex-col'>
         <div className='flex h-[37px] w-[100%] items-center justify-between'>
-          <img
-            className='h-[15px] w-[50px]'
-            src={Logo}
-            alt='ROOM:IT 로고'
-          />
+          <Link to='/'>
+            <img
+              className='h-[15px] w-[50px]'
+              src={Logo}
+              alt='ROOM:IT 로고'
+            />
+          </Link>
           <RiNotification3Line className='h-[24px] w-[24px]' />
         </div>
         <p className='flex h-[42px] w-[100%] items-center justify-center text-[18px] font-normal'>
