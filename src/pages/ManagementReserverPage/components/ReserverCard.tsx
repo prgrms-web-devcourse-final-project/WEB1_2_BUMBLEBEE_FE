0119452dcaa8955608178ed8518d3bfda4038e04
JOEIH <joh73459@gmail.com>
2024-12-07 01:33:29 +0900

    ✨ Feat : 결제 취소된 예약일 경우 사업자의 예약자 조회 페이지에서 결제 취소된 예약임을 표시

--- src/pages/ManagementReserverPage/components/ReserverCard.tsx
+++ src/pages/ManagementReserverPage/components/ReserverCard.tsx
@@ -17,62 +17,74 @@ const ReserverCard = ({ item }: { item: ReserverInfo }) => {
     workplaceImageUrl,
     workplaceId,
     reservationPrice,
+    reservationState,
   } = item;
 
+  console.log(reservationState);
+
   return (
-    <div className='mx-auto flex w-custom flex-col gap-2 border-b border-solid border-b-black px-[13px] py-[26px]'>
-      <div className='flex justify-between'>
-        <div className='flex flex-col items-start gap-4'>
-          <div className='flex cursor-pointer items-center gap-1.5 font-medium'>
-            <Link to={`/detail/${workplaceId}`}>{workplaceName}</Link>
-            <MdArrowForwardIos className='w-3' />
+    <>
+      <div className='relative mx-auto flex w-custom flex-col gap-2 border-b border-solid border-b-black px-[13px] py-[26px]'>
+        <div className='flex justify-between'>
+          <div className='flex flex-col items-start gap-4'>
+            <div className='flex cursor-pointer items-center gap-1.5 font-medium'>
+              <Link to={`/detail/${workplaceId}`}>{workplaceName}</Link>
+              <MdArrowForwardIos className='w-3' />
+            </div>
+
+            <ul className='flex flex-col gap-1 text-[12px]'>
+              <ListStyle
+                name='예약자명'
+                value={reservationName}
+              />
+              <ListStyle
+                name='전화번호'
+                value={reservationPhoneNumber}
+              />
+              <ListStyle
+                name='예약된 룸'
+                value={studyRoomName}
+              />
+              <ListStyle
+                name='예약일'
+                value={getDateFunction(reservationStartTime)}
+              />
+              <ListStyle
+                name='예약 시간'
+                value={`${getTimeFunction(reservationStartTime)} ~ ${getTimeFunction(reservationEndTime)}`}
+              />
+              <ListStyle
+                name='인원'
+                value={`${reservationCapacity}인`}
+              />
+              <ListStyle
+                name='결제일'
+                value={getDateFunction(reservationCreatedAt)}
+              />
+            </ul>
           </div>
 
-          <ul className='flex flex-col gap-1 text-[12px]'>
-            <ListStyle
-              name='예약자명'
-              value={reservationName}
+          <div className='flex flex-col justify-between'>
+            <img
+              src={workplaceImageUrl}
+              alt='스터디룸 사진'
+              className='h-[50px] w-[50px] object-cover'
             />
-            <ListStyle
-              name='전화번호'
-              value={reservationPhoneNumber}
-            />
-            <ListStyle
-              name='예약된 룸'
-              value={studyRoomName}
-            />
-            <ListStyle
-              name='예약일'
-              value={getDateFunction(reservationStartTime)}
-            />
-            <ListStyle
-              name='예약 시간'
-              value={`${getTimeFunction(reservationStartTime)} ~ ${getTimeFunction(reservationEndTime)}`}
-            />
-            <ListStyle
-              name='인원'
-              value={`${reservationCapacity}인`}
-            />
-            <ListStyle
-              name='결제일'
-              value={getDateFunction(reservationCreatedAt)}
-            />
-          </ul>
+            <span className='self-end text-sm font-normal'>
+              {reservationPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+              원
+            </span>
+          </div>
         </div>
-
-        <div className='flex flex-col justify-between'>
-          <img
-            src={workplaceImageUrl}
-            alt='스터디룸 사진'
-            className='h-[50px] w-[50px] object-cover'
-          />
-          <span className='self-end text-sm font-normal'>
-            {reservationPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
-            원
-          </span>
-        </div>
+        {reservationState === 'CANCELLED' && (
+          <div className='absolute left-0 top-0 flex h-[100%] w-[100%] flex-col items-center justify-center bg-white bg-opacity-90 text-[#E49E9E]'>
+            결제 취소된 예약입니다.
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
