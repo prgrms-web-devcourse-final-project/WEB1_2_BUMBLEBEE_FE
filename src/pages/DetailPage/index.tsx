242e3b105de846ffeb148753a85d50fb3e9bbca8
eunjju2 <graceun309@gmail.com>
2024-12-06 14:34:18 +0900

    ✨ Feat : 로딩 시 스피너 구현

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -2,6 +2,7 @@ import HeaderWithTitle from '@layouts/HeaderWithTitle';
 import MainLayout from '@layouts/MainLayout';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { SyncLoader } from 'react-spinners';
 import DetailNavigation from './components/DetailNavigation';
 import TabComponent from './components/TabComponent';
 import useGetWorkplaceData from './hooks/useGetWorkplaceData';
@@ -11,27 +12,29 @@ const DetailPage = () => {
   const { data, isLoading } = useGetWorkplaceData(Number(workplaceId));
   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
 
-  if (isLoading) {
-    return <p>로딩중</p>;
-  }
-
   return (
     <MainLayout headerType='both'>
       <HeaderWithTitle title={data?.workplaceName || 'Unknown'} />
-      <div className='pt-[10px]'>
-        <img
-          src={data?.imageUrl}
-          alt='사업장 사진'
-          className='h-[240px] w-[100%] object-cover'
-        />
-        {data && (
-          <TabComponent
-            setIsBtnDisabled={setIsBtnDisabled}
-            workplaceDetailData={data}
-            workplaceId={Number(workplaceId)}
+      {isLoading ? (
+        <div className='flex h-[calc(100vh-132px-94px)] w-full items-center justify-center'>
+          <SyncLoader color='#50BEAD' />
+        </div>
+      ) : (
+        <div className='pt-[10px]'>
+          <img
+            src={data?.imageUrl}
+            alt='사업장 사진'
+            className='h-[240px] w-[100%] object-cover'
           />
-        )}
-      </div>
+          {data && (
+            <TabComponent
+              setIsBtnDisabled={setIsBtnDisabled}
+              workplaceDetailData={data}
+              workplaceId={Number(workplaceId)}
+            />
+          )}
+        </div>
+      )}
       <DetailNavigation
         workplaceId={data?.workplaceId || 0}
         isBtnDisabled={isBtnDisabled}
