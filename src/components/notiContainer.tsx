92c7d3bca0ee048fe445a846740c353902d67c5b
JOEIH <joh73459@gmail.com>
2024-12-07 18:48:08 +0900

    ✨ Feat : 실시간 알림 수정 #104

--- src/components/notiContainer.tsx
+++ src/components/notiContainer.tsx
@@ -1,11 +1,20 @@
 import { AiFillNotification } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
-const NotiContainer = ({ message }: { message: string }) => {
+const NotiContainer = ({
+  message,
+  workplaceId,
+}: {
+  message: string;
+  workplaceId: number;
+}) => {
   return (
-    <div className='fixed top-8 z-[2000] flex h-[70px] w-custom items-center justify-start gap-3 rounded-lg bg-white px-4 py-2 text-base text-black shadow-custom'>
-      <AiFillNotification className='size-5 text-primary' />
-      {message}
-    </div>
+    <Link to={`/detail/${workplaceId}`}>
+      <div className='fixed left-[50%] top-8 z-[2000] flex h-[70px] w-custom translate-x-[-50%] items-center justify-start gap-3 rounded-lg bg-white px-4 py-2 text-base text-black shadow-custom'>
+        <AiFillNotification className='size-5 text-primary' />
+        {message}
+      </div>
+    </Link>
   );
 };
 
