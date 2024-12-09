f665ea1501f283a3af071837e77e7644dbd75b36
eunjju2 <graceun309@gmail.com>
2024-12-09 10:04:15 +0900

    💄 Design : 읽음처리 스타일 제거 #136

--- src/pages/ChatPage/components/SendMessage.tsx
+++ src/pages/ChatPage/components/SendMessage.tsx
@@ -15,9 +15,9 @@ const SendMessage = (props: SendMessageProps) => {
     <div className='flex w-custom justify-end'>
       <div className='flex items-end gap-2'>
         <div>
-          {!message.isRead && (
+          {/* {!message.isRead && (
             <p className='text-end text-xs text-primary'>1</p>
-          )}
+          )} */}
           <p className='text-xs text-subfont'>{formattedTime}</p>
         </div>
         <div className='max-w-60 rounded-t-xl rounded-bl-xl bg-primary px-3 py-2 text-sm text-white'>
